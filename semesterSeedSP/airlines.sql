e4264c6c6cc0eaa6e9e01e136527a81adb07bf27
cph57689 <pernillejaco@gmail.com>
2015-12-17 14:00:56 +0100

    sql file for airlines oprettet

---
+++ semesterSeedSP/airlines.sql
@@ -0,0 +1,8 @@
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://timetravel-tocvfan.rhcloud.com/','TimeTravel');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://semesterproject-martamiszczyk.rhcloud.com/semesterProject/','MPJ Air');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://wildfly-x.cloudapp.net/airline/','COSGrp2 Airline');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://jstairline-hardboilr.rhcloud.com/','JST Airlines');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://sargardon-001-site1.atempurl.com/','Norwegian');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://infamouslines-baniproductions.rhcloud.com','Infamous Lines');
+INSERT INTO AIRLINE (URL, AIRLINENAME) VALUES ('http://frenchyairlines-zerosource.rhcloud.com/','Frenchy Arilines');
+
